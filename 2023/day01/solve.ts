7eba3321480932dbe09e6bb10bd67e12974586e7
valerijmedvid <vmedvid@gmail.com>
2023-12-01 11:13:33 +0100

    add day01 of 2023

--- 2023/day01/solve.ts
+++ 2023/day01/solve.ts
@@ -5,22 +5,79 @@ const file = Bun.file(`${import.meta.dir}/${fileName}`)
 
 const inputData = await file.text()
 
-const inputDataArray = inputData
-  .split("\n\n")
-  .map((elf) => elf.split("\n").map((x) => parseInt(x)))
+const inputDataArray = inputData.split("\n")
 
-const sumArray = (myArray: number[]) => {
-  return myArray.reduce((prev, curr) => prev + curr, 0)
+const dict: string[] = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+]
+const numbers: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+function findFirstAndLastNumberFromStringArray(array: string[]) {
+  // filter out non-numbers
+  const arrayOfNumbers = array.filter(Number)
+
+  // find first and last number
+  const firstNumber = arrayOfNumbers[0]
+  const lastNumber = arrayOfNumbers[arrayOfNumbers.length - 1]
+
+  // return the sum
+  return parseInt(firstNumber + lastNumber)
 }
 
-// Get the cargo count for each elf
-const elvesCargoCount = inputDataArray.map((elf) => sumArray(elf))
+function findFirstAndLastNumberFromNumberArray(array: number[]) {
+  // find first and last number
+  const firstNumber = array[0]
+  const lastNumber = array[array.length - 1]
 
-// Sort the elvesCargoCount array in descending order
-const sortedElvesCargoCount = [...elvesCargoCount].sort((a, b) => b - a)
+  // return the sum
+  return firstNumber.toString() + lastNumber.toString()
+}
 
-// Get the top 3 most loaded elves
-const mostLoadedElves = sortedElvesCargoCount.slice(0, 3)
+// replace all numbers with their string counterparts
+function replaceStringNumbersWithNumbers(string: string) {
+  const numbersArray: number[] = []
+  for (let i = 0; i < string.length; i++) {
+    const newString = string.slice(i)
 
-console.log(Math.max(...elvesCargoCount))
-console.log(sumArray(mostLoadedElves))
+    // find first number in string
+    for (let num in dict) {
+      if (newString.startsWith(dict[num])) {
+        numbersArray.push(parseInt(num) + 1)
+      }
+    }
+    for (let num in dict) {
+      if (newString.startsWith(numbers[num])) {
+        numbersArray.push(parseInt(num) + 1)
+      }
+    }
+  }
+  return numbersArray
+}
+
+// Part 1
+console.log(
+  "Part 1: ",
+  inputDataArray
+    .map((line) => line.split(""))
+    .reduce((prev, curr) => {
+      return prev + findFirstAndLastNumberFromStringArray(curr)
+    }, 0)
+)
+
+// Part 2
+console.log(
+  "Part 2: ",
+  inputDataArray
+    .map((line) => replaceStringNumbersWithNumbers(line))
+    .reduce((prev, curr) => {
+      return prev + parseInt(findFirstAndLastNumberFromNumberArray(curr))
+    }, 0)
+)
